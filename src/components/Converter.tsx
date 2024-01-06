0a5466924ce6a9d61b17a482455871ae26b2109f
Maksym Volik <maksymvolik@gmail.com>
2024-01-06 15:12:24 +0200

    Added rearrangement of input fields

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,13 +1,15 @@
 import React, {useState, useContext} from 'react';
-import {View, Text, TextInput, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet, Pressable} from 'react-native';
 import {LevelContext} from '../context/LevelContext';
 import Separator from './Separator';
 import useThemeColors from '../hook/useThemeColors';
+import SVGImage from '../assets/icons/arrow-right-arrow-left.svg';
 
 const Converter: React.FC = () => {
   const colors = useThemeColors();
   const [firstValue, setFirstValue] = useState('');
   const [secondValue, setSecondValue] = useState('');
+  const [changePosition, setChangePosition] = useState(false);
   const {setLevel} = useContext(LevelContext);
 
   const onChangeFirstValue: (value: string) => void = value => {
@@ -36,6 +38,41 @@ const Converter: React.FC = () => {
     }
   };
 
+  const onPress = () => {
+    setChangePosition(!changePosition);
+  };
+
+  const inputBlock = (num: boolean) => {
+    return (
+      <View>
+        <View style={styles.inputBlock}>
+          <TextInput
+            style={[styles.input, {color: colors.text}]}
+            onChangeText={
+              !num
+                ? value => onChangeFirstValue(value)
+                : value => onChangeSecondValue(value)
+            }
+            value={!num ? firstValue : secondValue}
+            keyboardType="numeric"
+            textAlign="right"
+            maxLength={8}
+            autoFocus={!num}
+          />
+          <Text
+            style={[
+              styles.label,
+              {
+                color: colors.text,
+              },
+            ]}>
+            {!num ? 'мг/дл' : 'ммоль/л'}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={[styles.container, {backgroundColor: colors.background}]}>
       <Text
@@ -48,50 +85,27 @@ const Converter: React.FC = () => {
         Конвертор рівня цукру в крові
       </Text>
       <Separator />
-      <View>
-        <View style={styles.inputBlock}>
-          <TextInput
-            style={[styles.input, {color: colors.text}]}
-            onChangeText={value => onChangeFirstValue(value)}
-            value={firstValue}
-            keyboardType="numeric"
-            textAlign="right"
-            maxLength={8}
-            autoFocus={true}
+      {inputBlock(changePosition ? true : false)}
+      <View style={styles.pos}>
+        <Separator />
+        <Pressable
+          style={({pressed}) => [
+            {
+              backgroundColor: pressed ? colors.bgText : colors.background,
+              borderColor: colors.text,
+            },
+            styles.btn,
+          ]}
+          onPress={onPress}>
+          <SVGImage
+            style={{
+              transform: [{rotate: '90deg'}],
+            }}
+            fill={colors.text}
           />
-          <Text
-            style={[
-              styles.label,
-              {
-                color: colors.text,
-              },
-            ]}>
-            мг/дл
-          </Text>
-        </View>
+        </Pressable>
       </View>
-      <Separator />
-      <View>
-        <View style={styles.inputBlock}>
-          <TextInput
-            style={[styles.input, {color: colors.text}]}
-            onChangeText={value => onChangeSecondValue(value)}
-            value={secondValue}
-            keyboardType="numeric"
-            textAlign="right"
-            maxLength={8}
-          />
-          <Text
-            style={[
-              styles.label,
-              {
-                color: colors.text,
-              },
-            ]}>
-            ммоль/л
-          </Text>
-        </View>
-      </View>
+      {inputBlock(changePosition ? false : true)}
       <Separator />
     </View>
   );
@@ -111,6 +125,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
+  },
+  pos: {
+    position: 'relative',
+  },
+  btn: {
+    position: 'absolute',
+    left: 40,
+    top: -8,
+    width: 36,
+    height: 36,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 18,
+    borderWidth: 1,
   },
   input: {
     width: '100%',
